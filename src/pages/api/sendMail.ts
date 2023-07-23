ad1949cb8275a96c55dd6364ca57b7eec3fec538
Gutiiii <samuel.gutmans@gmail.com>
2023-07-23 11:55:41 +0200

    asd

--- src/pages/api/sendMail.ts
+++ src/pages/api/sendMail.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from "nodemailer";
 
 export const runtime = 'edge';
 
@@ -7,34 +6,60 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         const { name, email, message } = req.body;
 
-        const transporter = nodemailer.createTransport({
-            host: process.env.HOST,
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.USERNAME,
-                pass: process.env.PASSWORD
-            }
-        });
+        const formData = new URLSearchParams();
+        formData.append('from', "contact@sandbox8aba1c7fdf024caebb054691baabd496.mailgun.org");
+        formData.append('to', 'contact@samuel-gutmans.ch');
+        formData.append('subject', `Contact name: ${name}`);
+        formData.append('text', message);
 
         try {
-            const emailRes = await transporter.sendMail({
-                from: email,
-                to: "contact@samuel-gutmans.ch",
-                subject: `Contact name: ${name}`,
-                text: message
+            const emailRes = await fetch(`https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}/messages`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Basic ${Buffer.from(`api:${process.env.MAILGUN_API_KEY}`).toString('base64')}`,
+                },
+                body: formData
             });
 
-            console.log("Message sent: %s", emailRes.messageId);
+            if (!emailRes.ok) {
+                throw new Error(`Email not sent: ${emailRes.status}`);
+            }
 
-            res.status(200).json({ status: "Email sent" });
+            const emailResBody = await emailRes.json();
+
+            console.log("Message sent: %s", emailResBody.id);
+            return new Response(
+                JSON.stringify({
+                    name: 'Email sent',
+                }),
+                {
+                    status: 200,
+                }
+            )
+            // res.status(200).json({ status: "Email sent" });
         } catch (err) {
             console.log(err);
-            res.status(500).json({ status: "Email not sent" });
+            return new Response(
+                JSON.stringify({
+                    name: 'Email not sent',
+                }),
+                {
+                    status: 500,
+                }
+            )
+            // res.status(500).json({ status: "Email not sent" });
         }
 
     } else {
         // Handle any other HTTP method
-        res.status(200).json({ status: "not a post request" });
+        return new Response(
+            JSON.stringify({
+                name: 'not a post request',
+            }),
+            {
+                status: 200,
+            }
+        )
+        // res.status(200).json({ status: "not a post request" });
     }
 }
