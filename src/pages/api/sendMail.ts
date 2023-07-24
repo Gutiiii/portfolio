c8ff468b85da0651ea09974c61c64c623e58bceb
Gutiiii <samuel.gutmans@gmail.com>
2023-07-24 23:02:55 +0200

    asd

--- src/pages/api/sendMail.ts
+++ src/pages/api/sendMail.ts
@@ -3,65 +3,47 @@ import { NextRequest } from 'next/server';
 export const runtime = 'edge';
 
 export default async function POST(req: NextRequest) {
+    const body = await req.json()
 
-    return new Response("Hello!", {
-        status: 200,
-        statusText: "Alles klappt"
-    })
-    // if (req.method === 'POST') {
+    const { name, email, message } = body;
 
-    //     const body = await req.json()
+    const formData = new URLSearchParams();
+    formData.append('from', "contact@sandbox8aba1c7fdf024caebb054691baabd496.mailgun.org");
+    formData.append('to', 'samuel.gutmans@gmail.com');
+    formData.append('subject', "New Mail from Portfolio");
+    formData.append('text', `Contact name: ${name} \n Email Adresse: ${email} \n\n Message: ${message}`);
 
-    //     const { name, email, message } = body;
+    try {
+        const response = await fetch(`https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}/messages`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${Buffer.from(`api:${process.env.MAILGUN_API_KEY}`).toString('base64')}`,
+            },
+            body: formData,
+        });
+        if (response.status === 200) {
+            console.log("Message sent: %s", response);
+            return new Response(
+                JSON.stringify({
+                    name: 'Email sent',
+                }),
+                {
+                    status: 200,
+                }
+            )
+        } else {
+            throw new Error(`Email not sent: ${response.statusText}`);
+        }
 
-    //     const formData = new URLSearchParams();
-    //     formData.append('from', "contact@sandbox8aba1c7fdf024caebb054691baabd496.mailgun.org");
-    //     formData.append('to', 'samuel.gutmans@gmail.com');
-    //     formData.append('subject', "New Mail from Portfolio");
-    //     formData.append('text', `Contact name: ${name} \n Email Adresse: ${email} \n\n Message: ${message}`);
-
-    //     try {
-    //         const response = await fetch(`https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}/messages`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Authorization': `Basic ${Buffer.from(`api:${process.env.MAILGUN_API_KEY}`).toString('base64')}`,
-    //             },
-    //             body: formData,
-    //         });
-    //         if (response.status === 200) {
-    //             console.log("Message sent: %s", response);
-    //             return new Response(
-    //                 JSON.stringify({
-    //                     name: 'Email sent',
-    //                 }),
-    //                 {
-    //                     status: 200,
-    //                 }
-    //             )
-    //         } else {
-    //             throw new Error(`Email not sent: ${response.statusText}`);
-    //         }
-
-    //     } catch (err) {
-    //         console.log("Mailgun Error: " + err);
-    //         return new Response(
-    //             JSON.stringify({
-    //                 name: 'Email not sent',
-    //             }),
-    //             {
-    //                 status: 500,
-    //             }
-    //         )
-    //     }
-    // } else {
-    //     // Handle any other HTTP method
-    //     return new Response(
-    //         JSON.stringify({
-    //             name: 'not a post request',
-    //         }),
-    //         {
-    //             status: 200,
-    //         }
-    //     )
-    // }
+    } catch (err) {
+        console.log("Mailgun Error: " + err);
+        return new Response(
+            JSON.stringify({
+                name: 'Email not sent',
+            }),
+            {
+                status: 500,
+            }
+        )
+    }
 }
